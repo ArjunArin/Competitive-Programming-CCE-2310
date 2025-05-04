a3aff15b1d87025fdcc76219ca76929fe652910f
ArjunArin <arjunnahararin@gmail.com>
2025-05-05 03:37:50 +0600

    3rd commit

---
+++ Contest-1/c.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char C;
+    cin >> C;
+    if(C == 'z')
+    {
+      cout << 'a' << endl;
+    }
+    else
+    {
+      cout << char(C + 1) << endl;
+    }
+
+    return 0;
+}
---
+++ Contest-1/d.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long A, B, C, D;
+    cin >> A >> B >> C >> D;
+
+    if ((A + B - C == D) || (A + B * C == D) || (A - B + C == D) || 
+        (A - B * C == D) || (A * B + C == D) || (A * B - C == D)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
