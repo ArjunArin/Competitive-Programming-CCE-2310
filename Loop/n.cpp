e9cdf57b976c92f55dec514f80834acb0fb432cd
ArjunArin <arjunnahararin@gmail.com>
2025-05-26 00:15:24 +0600

    N,S,R added

---
+++ Loop/n.cpp
@@ -0,0 +1,24 @@
+// Problem Name : Numbers Histogram
+// Course Title : Competitive Programming Sessional
+// Course Code : CCE-2310
+// Course Teacher Name : Mirza Raquib
+// Date : 19/05/2025
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    char s;
+    long long n;
+    cin>>s>>n;
+    for(int i=0;i<n;i++) 
+    {
+        long long x;
+        cin>>x;
+        for(int j=0;j<x;j++) 
+        {
+            cout<<s;
+        }
+        cout<<endl;
+    }
+}
