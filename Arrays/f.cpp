1d8b6d01010d7c58758744df4ed69816b1f3e575
ArjunArin <arjunnahararin@gmail.com>
2025-06-01 20:49:34 +0600

    C,D,F are added

---
+++ Arrays/f.cpp
@@ -0,0 +1,22 @@
+// Problem Name : Reversing
+// Course Title : Competitive Programming Sessional
+// Course Code : CCE-2310
+// Course Teacher Name : Mirza Raquib
+// Date : 26/05/2025
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=n-1;i>=0;i--)
+{
+    cout<<arr[i]<<" ";
+}
+}
